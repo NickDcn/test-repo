a70c94701e0da2323ec75393100b4356428f47f0
NickDcn <92557484+NickDcn@users.noreply.github.com>
2021-10-17 22:03:20 +1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const api = express();
+const PORT = 8080;
+
+api.use(express.json())
+
+api.get('/test', (req, res) => { 
+    req.body
+    res.status(200).send({
+        test: "this a test"
+    })
+});
+
+api.listen(
+    PORT,   
+    () => console.log('API listening on port ${PORT}')
+)
